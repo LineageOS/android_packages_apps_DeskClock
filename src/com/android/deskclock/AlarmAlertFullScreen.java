c4b29d373508388a8bd714388e5461dd47a81add
Danesh Mondegarian <daneshm90@gmail.com>
2011-10-07 11:35:40 -0400

    AlarmClock : Lockscreen Fix
    http://code.google.com/p/cyanogenmod/issues/detail?id=3164
    
    Issue : The dismiss dialog would unlock the lockscreen.
    Resolution : Remove FLAG_DISMISS_KEYGUARD flag.
    
    Change-Id: I9920376a2f8195b80f2907052dbfe353c4cb54a7

--- src/com/android/deskclock/AlarmAlertFullScreen.java
+++ src/com/android/deskclock/AlarmAlertFullScreen.java
@@ -122,8 +122,7 @@ public class AlarmAlertFullScreen extends Activity implements GlowPadView.OnTrig
         mVolumeBehavior = Integer.parseInt(vol);
 
         final Window win = getWindow();
-        win.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
-                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+        win.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
         // Turn on the screen unless we are being launched from the AlarmAlert
         // subclass as a result of the screen turning off.
         if (!getIntent().getBooleanExtra(SCREEN_OFF, false)) {
