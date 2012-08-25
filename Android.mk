81ab77743b7b86b7beb4396a01b10c7b9d6e05f8
Jorge Ruesga <jorge@ruesga.com>
2012-08-25 04:23:49 +0200

    Request 5936: Stopwatch & Countdown
    Stopwatch and Countdown applications on Deskclock activity, request in issue 5936.
    The DeskClock is now a ViewPager, with StopWatch <-> DeskClock <-> CountDown fragments, allowing
    swiping between pages.
    
    Patch 2: Clean up
    Patch 3: BigTextStyle notifications, TimerHelper and extract titlebar color
    Patch 4: PowerManagement improvements:
               * CPU WakeLock in TimerClockService
               * New flags on startActivity on TimerClockService for determine if is necessary force turn on the screen
                 (force to use onCreate instead of onNewIntent to avoid that screen not turning on when user pressed the
                 power button previously)
               * Dim on StopWatch and CountDown after 30 seconds
             Correct a bug that not restart the UI thread when the both timers are running and a countdown is reached
    Patch 5: Improve layouts for big screens
    Patch 6: Use stat_notify_timer drawable for large icon notification
             ViewPager indicator (Trebuchet like)
    Patch 7: Brief tutorial of hidden options at first use (Trebuchet like)
             Remove WIP
    Patch 8: Use device density for resolve status bar height
    Patch 9: Improved layouts: now are based on screen width, not clock width
             Autoresize DigitalTimerClock to fit the parent view
             Fixed bugs
    
    Change-Id: Ib30b9cdc371f535d899b0b9794a2ac68a1d18fe5

--- Android.mk
+++ Android.mk
@@ -3,13 +3,16 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES := src/com/android/deskclock/ITimerClockService.aidl
+LOCAL_SRC_FILES += $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := DeskClock
 
 LOCAL_OVERRIDES_PACKAGES := AlarmClock
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.cfg
 
 include $(BUILD_PACKAGE)
 
