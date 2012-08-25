9eba8c0b4558cb68d5358047d244f778ac485279
Jorge Ruesga <jorge@ruesga.com>
2012-08-25 04:23:49 +0200

    Request 5936: Stopwatch & Countdown
    Stopwatch and Countdown applications on Deskclock activity, request in issue 5936.
    The DeskClock is now a ViewPager, with StopWatch <-> DeskClock <-> CountDown fragments, allowing
    swiping between pages.
    
    Patch 2: Clean up
    Patch 3: BigTextStyle notifications, TimerHelper and extract titlebar color
    Patch 4: PowerManagement improvements:
               * CPU WakeLock in TimerClockService
               * New flags on startActivity on TimerClockService for determine if is necessary force turn on the screen
                 (force to use onCreate instead of onNewIntent to avoid that screen not turning on when user pressed the
                 power button previously)
               * Dim on StopWatch and CountDown after 30 seconds
             Correct a bug that not restart the UI thread when the both timers are running and a countdown is reached
    Patch 5: Improve layouts for big screens
    Patch 6: Use stat_notify_timer drawable for large icon notification
             ViewPager indicator (Trebuchet like)
    Patch 7: Brief tutorial of hidden options at first use (Trebuchet like)
             Remove WIP
    Patch 8: Use device density for resolve status bar height
    Patch 9: Improved layouts: now are based on screen width, not clock width
             Autoresize DigitalTimerClock to fit the parent view
             Fixed bugs
    Patch 10: Fix milliseconds overflow on big numbers
              Set DeskClock fragment as the first page. StopWatch and CountDown are now at the right of DeskClock. Updated
                 tutorial to reflect the change.
              Change scrollbar indicator fade to 650 millis, like Trebuchet
              Fix more bugs
    Patch 11: The milliseconds of the clock were not adjusted because of a typo in the for loop
    
    Change-Id: Ib30b9cdc371f535d899b0b9794a2ac68a1d18fe5

---
+++ src/com/android/deskclock/StopWatchTimerClock.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.deskclock;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * An specialized class of {@link DigitalTimerClock} that implements an stopwatch timer.
+ */
+public class StopWatchTimerClock extends DigitalTimerClock {
+
+    /**
+     * Constructor of <code>StopWatchTimerClock</code>.
+     *
+     * @param context The current context
+     * @param attrs The attributes of the XML tag that is inflating the view.
+     */
+    public StopWatchTimerClock(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    /**
+     * Constructor of <code>StopWatchTimerClock</code>.
+     *
+     * @param context The current context
+     * @param attrs The attributes of the XML tag that is inflating the view.
+     * @param defStyle The default style to apply to this view. If 0, no style
+     *        will be applied (beyond what is included in the theme). This may
+     *        either be an attribute resource, whose value will be retrieved
+     *        from the current theme, or an explicit style resource.
+     */
+    public StopWatchTimerClock(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    /**
+     * Method that reset the time of the timer to 0.
+     */
+    public void resetTime() {
+        updateTimeOnUIThread(0);
+    }
+}
+
