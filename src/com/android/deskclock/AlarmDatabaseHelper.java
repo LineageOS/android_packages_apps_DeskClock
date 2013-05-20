cb0695d7d286d11b96bf1e938b51967f8ca2ccc8
Jorge Ruesga <jorge@ruesga.com>
2013-05-20 23:58:50 +0200

    DeskClock: Recreate alarms DB on downgrade
    This fixed the cm10 to cm10.1 transition (from version 7 to 6)
    
    Change-Id: Ia30915e8c4ebc36b191b5a876acbc8cb779b6689
    JIRA: CYAN-1188
    Issue: https://jira.cyanogenmod.org/browse/CYAN-1188
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/com/android/deskclock/AlarmDatabaseHelper.java
+++ src/com/android/deskclock/AlarmDatabaseHelper.java
@@ -76,6 +76,17 @@ class AlarmDatabaseHelper extends SQLiteOpenHelper {
         if (Log.LOGV) Log.v("Alarms database upgrade done.");
     }
 
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (Log.LOGV) Log.v("Downgrading alarms database from version " + oldVersion + " to "
+                + newVersion);
+
+        db.execSQL("DROP TABLE alarms;");
+        onCreate(db);
+
+        if (Log.LOGV) Log.v("Alarms database downgrade done.");
+    }
+
     Uri commonInsert(ContentValues values) {
         SQLiteDatabase db = getWritableDatabase();
         db.beginTransaction();
