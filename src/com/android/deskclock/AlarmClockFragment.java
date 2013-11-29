c46c19219ec19e9f1848ecc6759cad7681565504
Pavel Kirpichyov <pavel.kirpichyov@gmail.com>
2013-11-29 16:55:52 +0400

    [WIP] DeskClock: Add increasing volume option for alarm clocks
    http://review.cyanogenmod.org/#/c/31623/
    http://review.cyanogenmod.org/#/c/31798/
    
    Patchset 2: Fix database, layout, code style and check alarm volume
    
    Author: Austen Dicken <cvpcsm@gmail.com>
    Change-Id: I432f14d17aa8965a9267227a5ee892b6eab30caf

--- src/com/android/deskclock/AlarmClockFragment.java
+++ src/com/android/deskclock/AlarmClockFragment.java
@@ -721,6 +721,7 @@ public class AlarmClockFragment extends DeskClockFragment implements
             ViewGroup[] dayButtonParents = new ViewGroup[7];
             ToggleButton[] dayButtons = new ToggleButton[7];
             CheckBox vibrate;
+            CheckBox increasingVolume;
             TextView ringtone;
             View hairLine;
             View arrow;
@@ -1014,6 +1015,7 @@ public class AlarmClockFragment extends DeskClockFragment implements
                 holder.dayButtonParents[i] = viewgroup;
             }
             holder.vibrate = (CheckBox) view.findViewById(R.id.vibrate_onoff);
+            holder.increasingVolume = (CheckBox) view.findViewById(R.id.increasing_volume_onoff);
             holder.ringtone = (TextView) view.findViewById(R.id.choose_ringtone);
 
             view.setTag(holder);
@@ -1284,6 +1286,22 @@ public class AlarmClockFragment extends DeskClockFragment implements
                 @Override
                 public void onClick(View view) {
                     launchRingTonePicker(alarm);
+                }
+            });
+
+
+            itemHolder.increasingVolume.setVisibility(View.VISIBLE);
+            itemHolder.increasingVolume.setChecked(alarm.increasingVolume);
+            itemHolder.increasingVolume.setTextColor(
+                    alarm.increasingVolume ? mColorLit : mColorDim);
+            itemHolder.increasingVolume.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final boolean checked = ((CheckBox) v).isChecked();
+                    //When action mode is on - simulate long click
+                    itemHolder.increasingVolume.setTextColor(checked ? mColorLit : mColorDim);
+                    alarm.increasingVolume = checked;
+                    asyncUpdateAlarm(alarm, false);
                 }
             });
         }
