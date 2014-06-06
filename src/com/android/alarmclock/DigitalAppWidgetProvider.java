a46cb0774c5511c73a2bcd725854751131c11c1a
kaiyiz <kaiyiz@codeaurora.org>
2014-06-06 14:36:34 +0800

    DeskClock: Make clock widget display AM/PM as 12-hour format
    Lock screen clock widget dose not display AM/PM as 12-hour format,
    because the AM/PM font size is set to 0.
    
    Set the AM/PM font size to widget_label_font_size, then it displays
    AM/PM as 12-hour format.
    
    CRs-Fixed: 675785
    
    Change-Id: I93b3d0aa60c208907bc10a72d8bc277354269221

--- src/com/android/alarmclock/DigitalAppWidgetProvider.java
+++ src/com/android/alarmclock/DigitalAppWidgetProvider.java
@@ -94,7 +94,10 @@ public class DigitalAppWidgetProvider extends AppWidgetProvider {
                     RemoteViews widget = new RemoteViews(context.getPackageName(),
                             R.layout.digital_appwidget);
                     float ratio = WidgetUtils.getScaleRatio(context, null, appWidgetId);
-                    WidgetUtils.setTimeFormat(widget, 0/*no am/pm*/, R.id.the_clock);
+                    WidgetUtils.setTimeFormat(
+                            widget,
+                            (int) context.getResources().getDimension(
+                                    R.dimen.widget_label_font_size), R.id.the_clock);
                     WidgetUtils.setClockSize(context, widget, ratio);
                     refreshAlarm(context, widget);
                     appWidgetManager.partiallyUpdateAppWidget(appWidgetId, widget);
@@ -168,7 +171,9 @@ public class DigitalAppWidgetProvider extends AppWidgetProvider {
 
         // Setup alarm text clock's format and font sizes
         refreshAlarm(context, widget);
-        WidgetUtils.setTimeFormat(widget, 0/*no am/pm*/, R.id.the_clock);
+        WidgetUtils.setTimeFormat(widget,
+                (int) context.getResources().getDimension(R.dimen.widget_label_font_size),
+                R.id.the_clock);
         WidgetUtils.setClockSize(context, widget, ratio);
 
         // Set today's date format
