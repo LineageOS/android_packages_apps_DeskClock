4a677c6103c435c953385159b0da91653c89091f
jiangmin <jiangmin@codeaurora.org>
2014-11-12 12:35:59 -0800

    WorldClock: Add some worldwide cities and update timezone
    Add some cities info to xml.
    When local is changed, the timezone in setting page should be
    refreshed.
    
    Change-Id: If13beeb3c92227f67819f016e4631913fc6daca7

--- src/com/android/deskclock/SettingsActivity.java
+++ src/com/android/deskclock/SettingsActivity.java
@@ -18,6 +18,7 @@ package com.android.deskclock;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.media.AudioManager;
 import android.os.Bundle;
@@ -37,6 +38,7 @@ import com.android.deskclock.worldclock.Cities;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 import java.util.TimeZone;
 
 /**
@@ -73,6 +75,7 @@ public class SettingsActivity extends PreferenceActivity
 
 
     private static CharSequence[][] mTimezones;
+    private static Locale mLocale;
     private long mTime;
 
 
@@ -90,7 +93,7 @@ public class SettingsActivity extends PreferenceActivity
         // onResume() is called so we do it once in onCreate
         ListPreference listPref;
         listPref = (ListPreference) findPreference(KEY_HOME_TZ);
-        if (mTimezones == null) {
+        if (mTimezones == null || isLocaleChanged()) {
             mTime = System.currentTimeMillis();
             mTimezones = getAllTimezones();
         }
@@ -225,7 +228,13 @@ public class SettingsActivity extends PreferenceActivity
         pref.setOnPreferenceChangeListener(this);
 
         listPref = (ListPreference)findPreference(KEY_HOME_TZ);
+        if (mTimezones == null || isLocaleChanged()) {
+            mTime = System.currentTimeMillis();
+            mTimezones = getAllTimezones();
+        }
         listPref.setEnabled(state);
+        listPref.setEntryValues(mTimezones[0]);
+        listPref.setEntries(mTimezones[1]);
         listPref.setSummary(listPref.getEntry());
 
         listPref = (ListPreference) findPreference(KEY_VOLUME_BUTTONS);
@@ -316,4 +325,17 @@ public class SettingsActivity extends PreferenceActivity
         return timeZones;
     }
 
+    private boolean isLocaleChanged() {
+        Resources resource = getResources();
+        if ( resource != null ) {
+            Configuration config = resource.getConfiguration();
+            if ( config != null ) {
+                if ( mLocale == config.locale ) {
+                    return false;
+                }
+                mLocale = config.locale;
+            }
+        }
+        return true;
+    }
 }
