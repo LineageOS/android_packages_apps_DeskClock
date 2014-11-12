0d6cd77aaa3a18495ca8a8d0978dbd7a47433100
Pavel Kirpichyov <pavel.kirpichyov@gmail.com>
2014-11-12 12:35:58 -0800

    Provide upgrade path for cm-14.1 -> lineage-15.1
    This just takes care of making the database is up to date with cm-14.1
    
    * Increasing alarm and switch to profile still need to be implemented.
    
    Change-Id: Ibbb2b618ddeb6f93cd95a0fba3923bfd6d3fbbbb

--- Android.mk
+++ Android.mk
@@ -4,12 +4,15 @@ include $(CLEAR_VARS)
 LOCAL_RESOURCE_DIR := packages/apps/DeskClock/res
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_SDK_VERSION := current
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 LOCAL_PACKAGE_NAME := DeskClock
 LOCAL_OVERRIDES_PACKAGES := AlarmClock
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src gen)
+
+LOCAL_STATIC_JAVA_LIBRARIES += \
+    org.lineageos.platform.internal
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
         $(ANDROID_SUPPORT_DESIGN_TARGETS) \
