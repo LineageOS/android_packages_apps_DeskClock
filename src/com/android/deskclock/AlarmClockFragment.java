6900a665ea830ebcb493aa402b11eaf38e0485f9
Wilhelm Fitzpatrick <rafial@cyngn.com>
2015-12-15 13:45:06 -0800

    DeskClock: add increasing volume option for alarm clocks
    Based on CM12 feature:
    
    https://github.com/CyanogenMod/android_packages_apps_DeskClock/commit/7fc93e1b93b0e9e56a8426a6201a26c2bfca90ba
    http://review.cyanogenmod.org/#/c/82967/
    
    Change-Id: I090c76bd773e419f10e1e02bcf3c40ad04d6a988

--- src/com/android/deskclock/AlarmClockFragment.java
+++ src/com/android/deskclock/AlarmClockFragment.java
@@ -573,6 +573,7 @@ public abstract class AlarmClockFragment extends DeskClockFragment implements
             LinearLayout repeatDays;
             CompoundButton[] dayButtons = new CompoundButton[7];
             CheckBox vibrate;
+            CheckBox increasingVolume;
             TextView ringtone;
             View hairLine;
             View arrow;
@@ -713,6 +714,7 @@ public abstract class AlarmClockFragment extends DeskClockFragment implements
                 holder.dayButtons[i] = dayButton;
             }
             holder.vibrate = (CheckBox) view.findViewById(R.id.vibrate_onoff);
+            holder.increasingVolume = (CheckBox) view.findViewById(R.id.increasing_volume_onoff);
             holder.ringtone = (TextView) view.findViewById(R.id.choose_ringtone);
 
             view.setTag(holder);
@@ -1050,6 +1052,17 @@ public abstract class AlarmClockFragment extends DeskClockFragment implements
                 @Override
                 public void onClick(View view) {
                     launchRingTonePicker(alarm);
+                }
+            });
+
+            itemHolder.increasingVolume.setVisibility(View.VISIBLE);
+            itemHolder.increasingVolume.setChecked(alarm.increasingVolume);
+            itemHolder.increasingVolume.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final boolean checked = ((CheckBox) v).isChecked();
+                    alarm.increasingVolume = checked;
+                    asyncUpdateAlarm(alarm, false);
                 }
             });
         }
