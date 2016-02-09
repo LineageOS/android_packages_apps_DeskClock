8ca73f7304a8b9c9830bc45e8560f3fdbad08f59
Danesh M <daneshm90@gmail.com>
2016-02-09 11:44:48 -0800

    DeskClock: Fix "GoogleNow cancel alarms crash"
    The crash is caused by inconsistency between parcel writes/reads.
    writeToParcel includes attributes which were being ignored by the
    createFromParcel of ParcelUuid and hence setting the seek position in the parcel
    incorrectly and corrupting the next object. This is why the 2nd object was null.
    
    CYNGNOS-1696
    
    Change-Id: I5eda72002cc8e489accf185a64bbe84e4cd7614b

--- src/com/android/deskclock/provider/Alarm.java
+++ src/com/android/deskclock/provider/Alarm.java
@@ -323,7 +323,7 @@ public final class Alarm implements Parcelable, ClockContract.AlarmsColumns {
         alert = p.readParcelable(null);
         deleteAfterUse = p.readInt() == 1;
         increasingVolume = p.readInt() == 1;
-        profile = ParcelUuid.CREATOR.createFromParcel(p).getUuid();
+        profile = ((ParcelUuid) p.readParcelable(null)).getUuid();
     }
 
     public Alarm(Cursor c, Uri defaultRingtoneUri) {
