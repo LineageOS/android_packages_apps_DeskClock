a1de334aaf2aada20d48b459a64a668d895d78f5
Danesh M <daneshm90@gmail.com>
2016-02-23 11:04:08 -0800

    DeskClock : Don't set POWER_OFF_ALARM_MODE unecessarily
    Setting this even if alarm is cancelled can get the user in a state
    where the flag is never unset, and hence consume their home/search/menu keys...etc.
    
    CYNGNOS-2119
    
    Change-Id: I7b16de8530c937192d2c87b0ae52438185eca8d8

--- src/com/android/deskclock/alarms/AlarmActivity.java
+++ src/com/android/deskclock/alarms/AlarmActivity.java
@@ -205,7 +205,6 @@ public class AlarmActivity extends AppCompatActivity
             // if we are invoked by a power off alarm, we may have just missed the alarm
             // time if the boot took unusually long (we normally wake up two minutes early)
             mAlarmInstance = AlarmInstance.getFirstMissedInstance(mContext.getContentResolver());
-            Settings.System.putInt(mContext.getContentResolver(), POWER_OFF_ALARM_MODE, 1);
         } else if (intentData != null) {
             long instanceId = AlarmInstance.getId(intentData);
             mAlarmInstance = AlarmInstance.getInstance(this.getContentResolver(), instanceId);
@@ -230,6 +229,7 @@ public class AlarmActivity extends AppCompatActivity
                         SettingsActivity.DEFAULT_VOLUME_BEHAVIOR);
 
         if (mIsPowerOffAlarm) {
+            Settings.System.putInt(mContext.getContentResolver(), POWER_OFF_ALARM_MODE, 1);
             getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY);
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
                     | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
