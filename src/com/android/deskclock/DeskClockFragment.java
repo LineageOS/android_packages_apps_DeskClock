559c3a1cc7c91ce60e7f95fd06502ef23e667b8b
Michael W <baddaemon87@gmail.com>
2022-02-23 18:26:53 +0100

    DeskClock: Material Me? No YOU!
    * Since Android 12 introduced a new style, try to mimic it for this app
    * Due to AndroidX and the support libraries still being old, we have to,
      like Settings does with SettingsLib, create our own styles in the
      desired looks
    * Also adjust the color scheme to reflect Material You
    * We deviated from AOSP already quite a bit, so we don't need to really
      care about the change's footprint
    * Clean up behind us by removing (now unused) resources and includes in
      files that were modified anyways (and adjust order while on it)
    * Add constraintlayout and remove androidx.legacy* - only an unused
      import is left from these anyway
    * Import SettingsLib so we can access CollapsibleToolbarBaseActivity
      and don't need to recreate it ourselves
    
    Change-Id: I4c81e03c71f468a9e468426a8233ad96059cb05d

--- src/com/android/deskclock/DeskClockFragment.java
+++ src/com/android/deskclock/DeskClockFragment.java
@@ -17,8 +17,8 @@
 package com.android.deskclock;
 
 import android.view.KeyEvent;
-import android.widget.Button;
 import android.widget.ImageView;
+
 import androidx.annotation.ColorInt;
 import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
@@ -55,12 +55,12 @@ public abstract class DeskClockFragment extends Fragment implements FabContainer
     }
 
     @Override
-    public void onLeftButtonClick(@NonNull Button left) {
+    public void onLeftButtonClick(@NonNull ImageView left) {
         // Do nothing here, only in derived classes
     }
 
     @Override
-    public void onRightButtonClick(@NonNull Button right) {
+    public void onRightButtonClick(@NonNull ImageView right) {
         // Do nothing here, only in derived classes
     }
 
