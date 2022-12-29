21783f03c5a09c198360cb148e13e3ecea6eb56a
Michael W <baddaemon87@gmail.com>
2022-12-29 19:29:22 +0100

    DeskClock: Timer: Highlight the used input groups
    * It's easier to see where the current input is located
    * Follow google's design decision
    
    Change-Id: Ia71c56b7fbffa5f22c6dec679a5ea2db270953b5

--- src/com/android/deskclock/timer/TimerSetupView.java
+++ src/com/android/deskclock/timer/TimerSetupView.java
@@ -22,8 +22,10 @@ import static com.android.deskclock.FabContainer.FAB_SHRINK_AND_EXPAND;
 import android.content.Context;
 import android.content.res.Resources;
 import android.text.BidiFormatter;
+import android.text.SpannableString;
 import android.text.TextUtils;
 import android.text.format.DateUtils;
+import android.text.style.ForegroundColorSpan;
 import android.text.style.RelativeSizeSpan;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
@@ -194,12 +196,21 @@ public class TimerSetupView extends LinearLayout implements View.OnClickListener
         final int hours = mInput[5] * 10 + mInput[4];
 
         final UiDataModel uidm = UiDataModel.getUiDataModel();
-        mTimeView.setText(TextUtils.expandTemplate(mTimeTemplate,
+        SpannableString text = new SpannableString(TextUtils.expandTemplate(mTimeTemplate,
                 uidm.getFormattedNumber(hours, 2),
                 uidm.getFormattedNumber(minutes, 2),
                 uidm.getFormattedNumber(seconds, 2)));
 
         final Resources r = getResources();
+        int endIdx = text.length();
+        int startIdx = seconds > 0 ? 8 : endIdx;
+        startIdx = minutes > 0 ? 4 : startIdx;
+        startIdx = hours > 0 ? 0 : startIdx;
+        if (startIdx != endIdx) {
+            final int highlightColor = r.getColor(R.color.accent_color, getContext().getTheme());
+            text.setSpan(new ForegroundColorSpan(highlightColor), startIdx, endIdx, 0);
+        }
+        mTimeView.setText(text);
         mTimeView.setContentDescription(r.getString(R.string.timer_setup_description,
                 r.getQuantityString(R.plurals.hours, hours, hours),
                 r.getQuantityString(R.plurals.minutes, minutes, minutes),
