13fec09161a1ad11a1b31e33b9e7830961172aae
Michael W <baddaemon87@gmail.com>
2023-01-05 22:22:19 +0100

    DeskClock: Moar Timers!
    * Instead of using a ViewPager with fragments for each Timer,
      switch to using a Recyclerview
    * We can fit more than one Timer on the screen that way
    * Account for portrait and landscape by switching the scrolling direction
    * Account for tablets by showing 2 (portrait) or 3 (landscape)
      timers next to each other before starting a new row
    
    Change-Id: If8ee65d650e085e1ae99d55c9956768e178091af

---
+++ src/com/android/deskclock/timer/TimerClickHandler.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.deskclock.timer;
+
+import androidx.fragment.app.Fragment;
+
+import com.android.deskclock.LabelDialogFragment;
+import com.android.deskclock.R;
+
+import com.android.deskclock.data.Timer;
+import com.android.deskclock.events.Events;
+
+/**
+ * Click handler for an alarm time item.
+ */
+public final class TimerClickHandler {
+
+    private final Fragment mFragment;
+
+    public TimerClickHandler(Fragment fragment) {
+        mFragment = fragment;
+    }
+
+    public void onEditLabelClicked(Timer timer) {
+        Events.sendAlarmEvent(R.string.action_set_label, R.string.label_deskclock);
+        final LabelDialogFragment fragment = LabelDialogFragment.newInstance(timer);
+        LabelDialogFragment.show(mFragment.getParentFragmentManager(), fragment);
+    }
+}
