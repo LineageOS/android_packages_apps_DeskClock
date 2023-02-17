ab6b2b85a481cbde4e54f209c5c6660ef09a972b
Michael W <baddaemon87@gmail.com>
2023-02-17 20:18:55 +0100

    DeskClock: Don't delete expired timers
    * When pressing the stop icon, we expect the timer to stop, not for
      it to be deleted
    
    Change-Id: Ic1b35eb3124c0db610813f9d572bb0727b508e6b

--- src/com/android/deskclock/timer/ExpiredTimersActivity.java
+++ src/com/android/deskclock/timer/ExpiredTimersActivity.java
@@ -188,7 +188,7 @@ public class ExpiredTimersActivity extends BaseActivity {
         final View stopButton = timerItem.findViewById(R.id.play_pause);
         stopButton.setOnClickListener(v -> {
             final Timer timer1 = DataModel.getDataModel().getTimer(timerId);
-            DataModel.getDataModel().removeTimer(timer1);
+            DataModel.getDataModel().resetOrDeleteTimer(timer1, R.string.label_deskclock);
             removeTimer(timer1);
         });
 
