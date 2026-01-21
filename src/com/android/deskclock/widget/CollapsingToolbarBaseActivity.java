c1604deb56217bded123acb8105a0574ce470bc1
Inhishonor <inhishonor@protonmail.com>
2026-01-21 12:04:08 -0800

    DeskClock: Migrate to Material 3
    - Migrate to Material Dynamic Colors
    
    - Fully migrate away from app compat themes
    
    - Migrate Material 2 elements to 3
    Change-Id: I84598b5c9cc67fd43f0f7cf461b9930e6813a249

--- src/com/android/deskclock/widget/CollapsingToolbarBaseActivity.java
+++ src/com/android/deskclock/widget/CollapsingToolbarBaseActivity.java
@@ -20,6 +20,7 @@ import android.app.ActionBar;
 import android.graphics.BlendMode;
 import android.graphics.BlendModeColorFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,6 +32,7 @@ import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.fragment.app.FragmentActivity;
 
 import com.android.deskclock.R;
+import com.android.deskclock.ThemeUtils;
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 
@@ -69,8 +71,12 @@ public class CollapsingToolbarBaseActivity extends FragmentActivity {
             actionBar.setDisplayShowTitleEnabled(true);
             // We need this to have an always light back arrow
             BlendModeColorFilter filter = new BlendModeColorFilter(
-                    getColor(R.color.system_neutral1_50),
-                    BlendMode.SRC_ATOP);
+                    ThemeUtils.resolveColor(
+                            this,
+                            R.attr.colorOnSurface
+                    ),
+                    BlendMode.SRC_ATOP
+            );
             toolbar.getNavigationIcon().setColorFilter(filter);
         }
     }
