9211c7efa512cec11334844198e39e9d90b8bfe9
demo <demo14157@gmail.com>
2026-09-14 16:51:32 -0400

    DeskClock: Update bottom navigation to align with material component implementation
    Change-Id: I711738d2f7c579efbb7b63765eef9f54cf02eaf8

--- src/com/android/deskclock/DeskClock.java
+++ src/com/android/deskclock/DeskClock.java
@@ -41,7 +41,6 @@ import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
@@ -221,7 +220,7 @@ public class DeskClock extends BaseActivity
 
         mFragmentUtils = new FragmentUtils(this);
         // Mirror changes made to the selected tab into UiDataModel.
-        mBottomNavigation = findViewById(R.id.bottom_view);
+        mBottomNavigation = findViewById(R.id.bottom_navigation);
         mBottomNavigation.setOnItemSelectedListener(mNavigationListener);
 
         // Honor changes to the selected tab from outside entities.
